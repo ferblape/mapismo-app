1a3b23aa5ada3b365a92968a3e02f32d54613f8c
Fernando Blat <ferblape@gmail.com>
2012-03-12 18:41:00 -0300

    Specs for sessions controller

---
+++ spec/controllers/sessions_controller_spec.rb
@@ -0,0 +1,48 @@
+# coding: UTF-8
+
+require 'spec_helper'
+
+describe SessionsController do
+  describe "#create" do
+    let(:auth) do
+      OmniAuth::AuthHash.new(
+        'info' => {
+          'username' => 'blat',
+          'uid' => 1
+        },
+        'extra' => {
+          'access_token' => {
+            'token' => 'token',
+            'secret' => 'secret'
+          }
+        }
+      )
+    end
+    
+    it "should set as the current_user the user found in the authentication process" do
+      request.env['omniauth.auth'] = auth
+      
+      user = mock()
+      User.expects(:find).with(1).returns(user)
+      
+      post :create
+      subject.current_user.should == user
+      response.should be_redirect
+    end
+    
+    it "should set as the current_user from a user created" do
+      request.env['omniauth.auth'] = auth
+      
+      user = mock()
+      User.expects(:find).with(1).returns(nil)
+      User.expects(:create).with({
+        id: 1, username: 'blat', 
+        token: 'token', secret: 'secret'
+      }).returns(user)
+      
+      post :create
+      subject.current_user.should == user
+      response.should be_redirect
+    end
+  end
+end
