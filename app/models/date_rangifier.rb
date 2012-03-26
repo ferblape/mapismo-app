6175c3a5364154dbaf03833efeb1af6354ff8115
Fernando Blat <ferblape@gmail.com>
2012-03-26 17:53:42 -0300

    Refactor dates range to allow to parameterize step on each range Instagram has different steps depending on the number of days

--- app/models/date_rangifier.rb
+++ app/models/date_rangifier.rb
@@ -10,25 +10,41 @@ class DateRangifier
   # Returns an array of arrays of ranges
   # in ranges of 24 hours
   def range
-    ranges = []
-    if (@to-@from) / 3600 > 24
-      ranges.push([@from, @from.end_of_day])
+    if (@to - @from) / 3600 > 24
+      days_of_diff = (@to - @from) / (3600*24)
+      step = if days_of_diff < 7
+        24/12
+      elsif days_of_diff < 30
+        24/6
+      else
+        24/2
+      end
+      ranges = []
+      ranges += hours_range(@from, @from.end_of_day, step)
       current_date = @from.beginning_of_day
       begin
         current_date = current_date.tomorrow.beginning_of_day
-        ranges.push([current_date, current_date.end_of_day])
+        ranges += hours_range(current_date, current_date.end_of_day, step)
       end while (current_date.to_date < @to.yesterday.to_date)
       current_date = current_date.tomorrow
-      ranges.push([current_date.beginning_of_day, @to])
+      ranges += hours_range(current_date.beginning_of_day, @to, step)
+      return ranges
     else
-      h1 = @from.hour + 1
-      h2 = @to.hour - 1
-      ranges.push([@from, @from + 1.hour - @from.min.minutes - @from.sec])
-      h1.upto(h2) do |hour|
-        ranges.push([Time.utc(@from.year,@from.month,@from.day,hour,0,0), Time.utc(@from.year,@from.month,@from.day,hour+1,0,0)])
-      end
-      ranges.push([@to - @to.min.minutes - @to.sec, @to])
+      return hours_range(@from, @to, 1)
     end
+  end
+
+  private
+
+  def hours_range(from, to, step)
+    return [from, to] if(from + step.hours > to)
+
+    ranges = []
+    begin
+      end_range = from + step.hours
+      end_range = to if end_range > to
+      ranges.push([from, end_range])
+    end while (from += step.hours) < to
     ranges
   end
 
--- spec/models/date_rangifier_spec.rb
+++ spec/models/date_rangifier_spec.rb
@@ -3,23 +3,49 @@
 require 'spec_helper'
 
 describe DateRangifier do
-  it "should return a range of dates in steps of days if the period is longer than 24 hours" do
-    from = Time.utc(2010,10,13,10,00,00)
-    to   = Time.utc(2010,10,15,15,30,00)
-    result = DateRangifier.new(from, to).range
-    result.should include([from, from.end_of_day])
-    result.should include([Time.utc(2010,10,14,00,00,00).beginning_of_day, Time.utc(2010,10,14,23,59,59).end_of_day])
-    result.should include([to.beginning_of_day, to])
+  context "when the period is shorter than 7 days" do
+    it "should return a range of dates in steps of 2 hours" do
+      from = Time.utc(2010,10,13,10,20,00)
+      to   = Time.utc(2010,10,15,15,30,00)
+      result = DateRangifier.new(from, to).range
+      result.should include([Time.utc(2010,10,13,10,20,00),Time.utc(2010,10,13,12,20,00)])
+      result.should include([Time.utc(2010,10,13,12,20,00),Time.utc(2010,10,13,14,20,00)])
+      result.should include([Time.utc(2010,10,13,14,20,00),Time.utc(2010,10,13,16,20,00)])
+      result.should include([Time.utc(2010,10,13,16,20,00),Time.utc(2010,10,13,18,20,00)])
+      result.should include([Time.utc(2010,10,13,18,20,00),Time.utc(2010,10,13,20,20,00)])
+      result.should include([Time.utc(2010,10,13,20,20,00),Time.utc(2010,10,13,22,20,00)])
+      result.should include([Time.utc(2010,10,14,00,00,00),Time.utc(2010,10,14,02,00,00)])
+      result.should include([Time.utc(2010,10,14,02,00,00),Time.utc(2010,10,14,04,00,00)])
+      result.should include([Time.utc(2010,10,14,04,00,00),Time.utc(2010,10,14,06,00,00)])
+      result.should include([Time.utc(2010,10,14,06,00,00),Time.utc(2010,10,14,8,00,00)])
+      result.should include([Time.utc(2010,10,14,8,00,00),Time.utc(2010,10,14,10,00,00)])
+      result.should include([Time.utc(2010,10,14,10,00,00),Time.utc(2010,10,14,12,00,00)])
+      result.should include([Time.utc(2010,10,14,12,00,00),Time.utc(2010,10,14,14,00,00)])
+      result.should include([Time.utc(2010,10,14,14,00,00),Time.utc(2010,10,14,16,00,00)])
+      result.should include([Time.utc(2010,10,14,16,00,00),Time.utc(2010,10,14,18,00,00)])
+      result.should include([Time.utc(2010,10,14,18,00,00),Time.utc(2010,10,14,20,00,00)])
+      result.should include([Time.utc(2010,10,14,20,00,00),Time.utc(2010,10,14,22,00,00)])
+      result.should include([Time.utc(2010,10,15,00,00,00),Time.utc(2010,10,15,02,00,00)])
+      result.should include([Time.utc(2010,10,15,02,00,00),Time.utc(2010,10,15,04,00,00)])
+      result.should include([Time.utc(2010,10,15,04,00,00),Time.utc(2010,10,15,06,00,00)])
+      result.should include([Time.utc(2010,10,15,06,00,00),Time.utc(2010,10,15,8,00,00)])
+      result.should include([Time.utc(2010,10,15,8,00,00),Time.utc(2010,10,15,10,00,00)])
+      result.should include([Time.utc(2010,10,15,10,00,00),Time.utc(2010,10,15,12,00,00)])
+      result.should include([Time.utc(2010,10,15,12,00,00),Time.utc(2010,10,15,14,00,00)])
+      result.should include([Time.utc(2010,10,15,14,00,00),Time.utc(2010,10,15,15,30,00)])
+    end
   end
 
-  it "should return a range of dates in steps of hours if the period is shorter than 24 hours" do
-    from = Time.utc(2010,10,15,10,10,00)
-    to   = Time.utc(2010,10,15,15,30,00)
-    result = DateRangifier.new(from, to).range
-    result.should include([from, Time.utc(2010,10,15,11,00,00)])
-    11.upto(14) do |hour|
-      result.should include([Time.utc(2010,10,15,hour,00,00), Time.utc(2010,10,15,hour+1,00,00)])
+  context "when the period is shorter than 24 hours" do
+    it "should return a range of dates in steps of 1 hour" do
+      from = Time.utc(2010,10,15,10,10,00)
+      to   = Time.utc(2010,10,15,15,30,00)
+      result = DateRangifier.new(from, to).range
+      result.should include([from, Time.utc(2010,10,15,11,10,00)])
+      11.upto(14) do |hour|
+        result.should include([Time.utc(2010,10,15,hour,10,00), Time.utc(2010,10,15,hour+1,10,00)])
+      end
+      result.should include([Time.utc(2010,10,15,15,10,00), to])
     end
-    result.should include([Time.utc(2010,10,15,15,00,00), to])
   end
 end
